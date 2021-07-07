4600fdec3bb43ac881cd966d5e2ac0c2f529dd1e
Tesfahun23 <tesfahunkebede336@gmail.com>
2021-07-08 01:42:42 +0300

    rm demo code

--- lib/payment/demo.dart
+++ lib/payment/demo.dart
@@ -1,19 +1,1 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
-class ThePaymentDemo extends StatefulWidget {
-  ThePaymentDemo({Key key}) : super(key: key);
-
-  @override
-  _ThePaymentDemoState createState() => _ThePaymentDemoState();
-}
-
-class _ThePaymentDemoState extends State<ThePaymentDemo> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child:
-                CupertinoButton(child: Text("PAY BIATCH"), onPressed: () {})));
-  }
-}
